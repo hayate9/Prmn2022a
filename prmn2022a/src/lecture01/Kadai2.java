ef48efebb68d9bdb86c1e0cdd5fb662ef3dde050
hayate9 <b2211470@photon.chitose.ac.jp>
2022-11-01 23:54:25 +0900

    課題1-2作成

---
+++ prmn2022a/src/lecture01/Kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai2 {
+    public static void main(String[] args) {
+        int studentNumber = 2211470;
+        System.out.println("B"+studentNumber+" Hayate Takahashi");
+    }
+}
