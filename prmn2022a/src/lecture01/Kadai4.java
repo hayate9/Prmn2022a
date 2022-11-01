ff459f95b7404e948a6d8e5174637584ce0da9aa
hayate9 <b2211470@photon.chitose.ac.jp>
2022-11-02 00:15:52 +0900

    課題1-4作成

---
+++ prmn2022a/src/lecture01/Kadai4.java
@@ -0,0 +1,17 @@
+package lecture01;
+
+public class Kadai4 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        int sum = 0;
+
+        for(int i = 0;i < array.length;i++){
+            array[i] = i + 1;
+
+            if(i % 2 == 0){
+                sum += array[i];
+            }
+        }
+        System.out.println("sum = "+sum);
+    }
+}
