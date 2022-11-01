b2e477f9d38a11853c185d9eb2afc73e59a43249
hayate9 <b2211470@photon.chitose.ac.jp>
2022-11-02 00:03:57 +0900

    課題1-3作成

---
+++ prmn2022a/src/lecture01/Kadai3.java
@@ -0,0 +1,17 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class Kadai3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How old are you?:");
+        int age = input.nextInt();
+
+        if(age < 20){
+            System.out.println("I am "+age+" years old so I cannot drink liquor.");
+        }else{
+            System.out.println("I am "+age+" years old so I can drink liquor.");
+        }
+    }
+}
